169f61c10595c9bc750bc0dc7a20a8ca99ab59d9
Rutger <rutger@nhtcu.nl>
2022-12-02 12:10:22 +0100

    over-reduced day02 with a lot of modulo calculus.

--- days/day02.cs
+++ days/day02.cs
@@ -12,38 +12,16 @@ internal class Day02 : Day
 
     public override long RunPart(int part, string inputName)
     {
-        var lines = GetListOfLines(inputName);
-        if (part == 1)
+        var lines = GetListOfLines(inputName)
+                .Select(x => new {L = Translate(x[0]), R = Translate(x[2])});
+        
+        if (part == 1) return lines.Select(x => x.R + 1 + ((1 - x.L + x.R) % 3) * 3).Sum();
+        
+        return lines.Select(x => (x.L + x.R + 2) % 3 + 1 + x.R * 3).Sum();
+
+        int Translate(char i)
         {
-            var scores = new Dictionary<string, int>()
-            {
-                {"A X", 1 + 3},
-                {"A Y", 2 + 6},
-                {"A Z", 3 + 0},
-                {"B X", 1 + 0},
-                {"B Y", 2 + 3},
-                {"B Z", 3 + 6},
-                {"C X", 1 + 6},
-                {"C Y", 2 + 0},
-                {"C Z", 3 + 3},
-            };
-            return lines.Select(x => scores[x]).Sum();
-        }
-        else
-        {
-            var scores = new Dictionary<string, int>()
-            {
-                {"A X", 3 + 0},
-                {"A Y", 1 + 3},
-                {"A Z", 2 + 6},
-                {"B X", 1 + 0},
-                {"B Y", 2 + 3},
-                {"B Z", 3 + 6},
-                {"C X", 2 + 0},
-                {"C Y", 3 + 3},
-                {"C Z", 1 + 6},
-            };
-            return lines.Select(x => scores[x]).Sum();
+            return (i - 'A') % ('X' - 'A');
         }
     }
 }
